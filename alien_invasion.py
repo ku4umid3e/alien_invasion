ddb55446200e0993693dcd482add88c2b0b0a8ad
ku4umid3e <kuchumid3e@gmail.com>
2020-11-19 23:37:39 +0300

    The screen options were put in a separate module 'settings.py'

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,25 @@
+from settings import Settings
 import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
     #Initializes the game and creates a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-
+    #Starting the main game loop
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        screen.fill(ai_settings.bg_color)
         
         pygame.display.flip()
 
