0b8b2bbe4a26009e1cd2514c497f616b68272d5b
ku4umid3e <kuchumid3e@gmail.com>
2020-11-23 09:26:08 +0300

    Taught the ship to react to the left and right keys

--- ship.py
+++ ship.py
@@ -16,11 +16,14 @@ class Ship:
         self.rect.bottom = self.screen_rect.bottom
         # Movement flag
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """ Update the ship's position, based on movement flags."""
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         """Draws the ship at the current position."""
