b1d025d04227a4534e1d0a0fd3c8bea2d7d0cda6
ku4umid3e <kuchumid3e@gmail.com>
2020-11-24 20:36:26 +0300

    Added bullet parametrs

--- settings.py
+++ settings.py
@@ -6,5 +6,13 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        
         # Settings ship
         self.ship_speed_factor = 1.5
+        
+        # Bullet parametrs
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        
