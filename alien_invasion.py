4456dbba23d8378f95500daf66cd10a3136e88ba
ku4umid3e <kuchumid3e@gmail.com>
2021-06-15 15:49:07 +0300

    Instantiating the Scoreboard.

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from ship import Ship
 import game_functions as gf
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 
 def run_game():
@@ -20,8 +21,9 @@ def run_game():
     # Created button "Play"
     play_button = Button(ai_settings, screen, "Play")
     
-    #Creating an instance for storing the game
+    #Creating an instance for GameStats and Scoreboard.
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     ship = Ship(ai_settings, screen)
     
     # Created group of a store bullets.
@@ -44,7 +46,8 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, 
+                bullets, play_button)
 
 
 run_game()
