eef8454d410fc3a92f80c8c0ed33bc83124e0e53
ku4umid3e <kuchumid3e@gmail.com>
2021-06-29 21:57:25 +0300

    Added an attribute for level output

--- game_stats.py
+++ game_stats.py
@@ -18,3 +18,4 @@ class GameStats():
         """Initializes statistics that change during the game."""
         self.ship_left = self.ai_settings.ship_limit
         self.score = 0
+        self.level = 1
